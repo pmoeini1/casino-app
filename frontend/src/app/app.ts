cc3cff7067e431266a344e76d8f62db62c3ccea7
pmoeini1 <pmoeini@uwo.ca>
2025-09-08 11:36:59 -0400

    add basic form

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true, 
 })
 export class App {
+  email: string = '';
+  password: string = '';
   protected readonly title = signal('frontend');
+  onSubmit() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
 }
