8a0e377d00a53099ef2da8436f2cbddd6ff02178
pmoeini1 <pmoeini@uwo.ca>
2025-09-11 09:52:28 -0400

    bet amount for slot machine

--- frontend/src/app/slotMachine.component.ts
+++ frontend/src/app/slotMachine.component.ts
@@ -5,12 +5,13 @@ import { Store } from '@ngxs/store';
 import { Increment, Decrement } from './state/counter.actions';
 import { CounterStateModel } from './state/counter.state';
 import { AsyncPipe, CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-slot-machine',
   templateUrl: './slotMachine.component.html',
   standalone: true,
-  imports: [CommonModule, AsyncPipe]
+  imports: [CommonModule, AsyncPipe, FormsModule]
 })
 export class SlotMachine {
   private store = inject(Store); 
@@ -19,6 +20,16 @@ export class SlotMachine {
   count$: Observable<number> = this.store.select(
     (state: { counter: CounterStateModel }) => state.counter.count
   );
+  wager:number=1;
+  message:string = 'Place your bet!';
+  setWager(amount: number) {
+    if (amount > 0 && amount <= (this.store.selectSnapshot((state: { counter: CounterStateModel }) => state.counter.count))) {
+        this.wager = amount;
+        this.message = `Wager set to ${this.wager}. Now spin the slots!`;
+    } else {
+        this.message = 'Invalid wager amount!';
+    }
+    }
 
   slot1: number = 0;
   slot2: number = 0;
@@ -32,10 +43,10 @@ export class SlotMachine {
 
     if (this.slot1 === this.slot2 && this.slot2 === this.slot3) {
       this.result = '🎉 You win!';
-      this.store.dispatch(new Increment(99));
+      this.store.dispatch(new Increment(99*this.wager));
     } else {
       this.result = '😢 You lose!';
-      this.store.dispatch(new Decrement(1));
+      this.store.dispatch(new Decrement(1*this.wager));
     }
   }
 
